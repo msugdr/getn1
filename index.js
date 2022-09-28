b373af76b510db7095648e0b71fe68a97b5ffc69
msugdr <msugdr@gmail.com>
2022-09-28 11:49:30 +0900

    Dear2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const request = require('request');
 const msg ="Dear YouNa, I Love You and Elena! Are You Happy?";
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
