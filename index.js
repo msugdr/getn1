94872b04fcfa0e174bfe223f92af03d3ff19f862
msugdr <msugdr@gmail.com>
2022-10-03 11:33:25 +0900

    10031133

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const msg ="Dear YouNa, I Love You and Elena! Request";
+const msg ="MESSAGE = ";
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end(msg);
