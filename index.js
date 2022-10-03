5acb720cb260ecea509a616e93a36b2f32294352
msugdr <msugdr@gmail.com>
2022-10-03 13:09:56 +0900

    1309

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
-var msg ="MESSAGE = ";
+var msg;
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-msg += request.url;
+msg = "MSG : " + request.url;
     response.end(msg);
 });
 
