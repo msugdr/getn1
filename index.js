cc8c42d281cb9d93948fc68cd9aca2d9b35efdce
msugdr <99190980+msugdr@users.noreply.github.com>
2023-04-04 08:18:06 +0900

    response.write(msg)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var msg;
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
 msg = " URI : " + request.url;
-    
+    response.write(msg);
     response.end(msg);
 });
 
