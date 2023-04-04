eece40d8eb86365111e780363e178f4c0d0a2f39
msugdr <99190980+msugdr@users.noreply.github.com>
2023-04-04 09:05:05 +0900

    back quote

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require('http');
+const port = process.env.PORT || 1337;
 var msg;
 const server = http.createServer((request, response) => {
-    msg = "<h1>ELENA</h1> URI = " + request.url + "<br>process.env.PORT = " + process.env.PORT;
+    msg = `<h1 style='color:red;'>ELENA</h1> URI = ` + request.url + "<br>process.env.PORT = " + process.env.PORT;
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(msg);
     response.end();
 });
 
-const port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+// console.log("Server running at http://localhost:%d", port);
