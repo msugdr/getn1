7d27d23c4dd09fb9dbfeffdd55a30bbd3025e0c8
msugdr <99190980+msugdr@users.noreply.github.com>
2023-04-04 16:27:34 +0900

    back

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const http = require('http');
 var fs = require('fs');
 const port = process.env.PORT || 1337;
 var msg;
-//const server = http.createServer((request, response) => {
-   // msg = `<h1 style='color:red;'>ELENA</h1><img src='girl_evergarten.jpg'><br> URI = ` + request.url + "<br>process.env.PORT = " + process.env.PORT;
-//    response.writeHead(200, {"Content-Type": "text/html"});
-//    response.write(msg);
-//    response.end();
-//});
-const server = http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+const server = http.createServer((request, response) => {
+    msg = `<h1 style='color:red;'>ELENA</h1> URI = ` + request.url + "<br>process.env.PORT = " + process.env.PORT;
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(msg);
+    response.end();
+});
+//const server = http.createServer(function (req, res) {
+//  fs.readFile('demofile1.html', function(err, data) {
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.write(data);
+//    return res.end();
+ // });
 
 server.listen(port);
 
